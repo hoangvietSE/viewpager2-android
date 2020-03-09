7ad0d6eeb641d46f1a9a971058254bc3aa88f6ba
HoangVietSE <viethoangtien@gmail.com>
2020-03-09 14:48:34 +0700

    Layout container for adapter

--- app/src/main/java/com/soict/hoangviet/viewpager2/DemoViewPagerAdapter.kt
+++ app/src/main/java/com/soict/hoangviet/viewpager2/DemoViewPagerAdapter.kt
@@ -5,32 +5,35 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.item_view_pager.*
 import kotlinx.android.synthetic.main.item_view_pager.view.*
 
 class DemoViewPagerAdapter : RecyclerView.Adapter<DemoViewPagerAdapter.EventViewHolder>() {
     val eventList = listOf("0", "1", "2")
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EventViewHolder {
-        return EventViewHolder(
-            LayoutInflater.from(parent.context).inflate(
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        EventViewHolder(LayoutInflater.from(parent.context).inflate(
                 R.layout.item_view_pager,
                 parent,
                 false
             )
         )
-    }
 
     override fun getItemCount() = eventList.size
 
     override fun onBindViewHolder(holder: EventViewHolder, position: Int) {
-        holder.itemView.apply {
-            tvPage.text = "Page ${position}"
+        holder.bind()
+    }
+
+    class EventViewHolder(override val containerView: View?) :
+        RecyclerView.ViewHolder(containerView!!), LayoutContainer {
+        fun bind() {
+            tvPage.text = "Page ${adapterPosition}"
             cslPage.background = ContextCompat.getDrawable(
-                context, if (position % 2 == 0)
+                containerView!!.context, if (adapterPosition % 2 == 0)
                     R.color.colorBlue else
                     R.color.colorOrangeYellow
             )
         }
     }
-
-    class EventViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 }
--- app/src/main/java/com/soict/hoangviet/viewpager2/MainActivity.kt
+++ app/src/main/java/com/soict/hoangviet/viewpager2/MainActivity.kt
@@ -3,7 +3,9 @@ package com.soict.hoangviet.viewpager2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.content.ContextCompat
+import androidx.viewpager.widget.ViewPager
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.item_view_pager.*
@@ -14,7 +16,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        viewpager.adapter = ViewPagerAdapter(supportFragmentManager, lifecycle)
+        viewpager.adapter = DemoViewPagerAdapter()
+        viewpager.orientation = ViewPager2.ORIENTATION_VERTICAL
         // Set the icon and text for each tab
         TabLayoutMediator(tabLayout, viewpager) { tab, position ->
             tab.text = "Tab ${position}"
